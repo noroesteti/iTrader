fcd00dda6a2b4a796739a941ad4dcb0d78aac6a2
mtzcpd910 <brunocalcagno@cvccorp.cvc.com.br>
2020-05-02 10:44:08 -0300

    Iniciar projetos no git

---
+++ src/main/java/com/noroesteti/iTrader/api/ITraderApplication.java
@@ -0,0 +1,13 @@
+package com.noroesteti.iTrader.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noroesteti/iTrader/api/ITraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noroesteti.iTrader.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
